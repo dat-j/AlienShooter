3365212c7aa62fdae0e551fceb86c0d67e1b3227
IRONHIVE <tranxuandat.dev@gmail.com>
2026-09-04 16:30:48 +0700

    chore: .gitkeep còn lại và metadata

--- tests/test_smoke.gd
+++ tests/test_smoke.gd
@@ -4,14 +4,14 @@ extends GutTest
 
 
 func test_gut_chay_duoc() -> void:
-	assert_true(true, "khung test GUT phải chạy")
+    assert_true(true, "khung test GUT phải chạy")
 
 
 func test_project_dung_ten() -> void:
-	var ten: String = str(ProjectSettings.get_setting("application/config/name"))
-	assert_eq(ten, "IRONHIVE", "tên project phải là IRONHIVE")
+    var ten: String = str(ProjectSettings.get_setting("application/config/name"))
+    assert_eq(ten, "IRONHIVE", "tên project phải là IRONHIVE")
 
 
 func test_renderer_la_forward_plus() -> void:
-	var rm: String = str(ProjectSettings.get_setting("rendering/renderer/rendering_method"))
-	assert_eq(rm, "forward_plus", "renderer phải là Forward+ theo TDD §1")
+    var rm: String = str(ProjectSettings.get_setting("rendering/renderer/rendering_method"))
+    assert_eq(rm, "forward_plus", "renderer phải là Forward+ theo TDD §1")
